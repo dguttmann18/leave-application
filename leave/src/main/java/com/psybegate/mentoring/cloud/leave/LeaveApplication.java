2b1333c58e03f17a05cdac2cf40df7cff0c66c31
dguttmann18 <97879065+dguttmann18@users.noreply.github.com>
2026-09-17 08:28:42 +0200

    Add files via upload

---
+++ leave/src/main/java/com/psybegate/mentoring/cloud/leave/LeaveApplication.java
@@ -0,0 +1,13 @@
+package com.psybegate.mentoring.cloud.leave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveApplication.class, args);
+	}
+
+}
